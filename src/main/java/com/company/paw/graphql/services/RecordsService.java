47f0b8249ce4ff0f2af7efd597e06de65d171e51
Amir <cnooed@gmail.com>
2019-06-08 04:20:06 -0400

    Refactoring Fix

--- src/main/java/com/company/paw/graphql/services/RecordsService.java
+++ src/main/java/com/company/paw/graphql/services/RecordsService.java
@@ -1,13 +1,17 @@
 package com.company.paw.graphql.services;
 
 import com.company.paw.Repositories.*;
-import com.company.paw.models.Record;
+import com.company.paw.graphql.InputTypes.RecordInput;
+import com.company.paw.models.*;
 import io.leangen.graphql.annotations.GraphQLMutation;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -30,12 +34,39 @@ public class RecordsService {
     }
 
     @GraphQLMutation
-    public Record addRecord(String id) {
-
-        return recordRepository.findById(id).orElse(null);
+    public Record addRecord(RecordInput input) {
+        return recordRepository.save(addInput(input));
     }
 
-    public List<Record> recordsIdToRecords(List<String> recordsId) {
+    private Record addInput(RecordInput input) {
+        Optional<Employee> employeeOptional = employeeRepository.findById(input.getUserId());
+        Optional<Weapon> weaponOptional = weaponRepository.findById(input.getProductId());
+        Optional<Plate> plateOptional = plateRepository.findById(input.getProductId());
+        Optional<Organization> organizationOptional = organizationRepository.findById(input.getOrganizationId());
+        Date date = null;
+        try {
+            date = new SimpleDateFormat("yyyy/MM/dd").parse(input.getTime());
+        } catch (Exception ignored) {
+        }
+
+        Record record = new Record();
+        record.setUser(employeeOptional.orElse(null));
+
+        if (weaponOptional.isPresent())
+            record.setProduct(weaponOptional.get());
+        else
+            record.setProduct(plateOptional.orElse(null));
+
+        record.setOrganization(organizationOptional.orElse(null));
+        record.setTime(date);
+        record.setStatus(input.isStatus());
+        record.setReturning(input.isReturning());
+        record.setDescription(input.getDescription());
+
+        return record;
+    }
+
+    List<Record> recordsIdToRecords(List<String> recordsId) {
         return recordsId.stream()
                 .map(record -> recordRepository.findById(record).orElse(null))
                 .collect(Collectors.toList());
