b86e9ab27d7acfeee7843667d90f8b5f5504e8fc
Amir <cnooed@gmail.com>
2019-06-08 03:49:36 -0400

    Added Upload Support

--- src/main/java/com/company/paw/PawApplication.java
+++ src/main/java/com/company/paw/PawApplication.java
@@ -1,11 +1,16 @@
 package com.company.paw;
 
+import com.company.paw.payload.models.UploadedPicture;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 import java.util.Date;
 
+@EnableConfigurationProperties({
+        UploadedPicture.class
+})
 @SpringBootApplication
 @EnableMongoAuditing
 public class PawApplication {
--- src/main/java/com/company/paw/graphql/InputTypes/CityInput.java
+++ src/main/java/com/company/paw/graphql/InputTypes/CityInput.java
@@ -1,5 +1,12 @@
 package com.company.paw.graphql.InputTypes;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CityInput {
     private String name;
     private String stateId;
